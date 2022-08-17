0d5795491456aba8be8a2e6ffb8a3333eac740f2
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-08-18 00:51:09 +0300

    commit tree d70ceeaa94821c01a8271fe3dec57c24663670b5

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,33 @@
-## code your solution here. 
+## code your solution here.
+require 'pry'
+class Cat 
+   attr_accessor :name
+ def meow
+  puts "meow!"
+ end
+end
+
+meow = Cat.new
+meow.name = "meow!"
+puts meow.name
+
+
+
+
+=begin 
+class Person
+  attr_reader :first_name,:last_name
+
+   def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+   def name
+    "#{@first_name} #{@last_name}".strip
+  end
+end
+jay_z = Person.new
+jay_z.name = "Shawn Carter"
+puts jay_z.name
+=end
